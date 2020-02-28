c5ceb1270fdaa6bc8f724d40e7c9c6df5a132b9f
UlrichMabou <ulrichmabou@gmail.com>
2020-02-28 20:40:42 +0300

    start with empty scaffold

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Friendlychat',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Friendlychat'),
+        ),
+      ),
+    ),
+  );
+}
